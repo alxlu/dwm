ac8cfc90424ecd68dc8c165bb92f1002752359b1
Alex Lu <alxlu@users.noreply.github.com>
2020-07-11 23:01:24 -0700

    fix dmenucmd

--- config.h
+++ config.h
@@ -75,7 +75,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 /*static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL }; */
-static const char *dmenucmd[] = { "rofi", "-show", "run", "-font", "PragmataPro Liga 13", "--dpi", "120", "-lines", "12", "-padding", "18", "-width", "60", "-location", "0", "-columns", "3", "-sidebar-mode" };
+static const char *dmenucmd[] = { "rofi", "-show", "run", "-font", "PragmataPro Liga 13", "-dpi", "120", "-lines", "12", "-padding", "18", "-width", "60", "-location", "0", "-columns", "3", "-sidebar-mode", NULL };
 static const char *termcmd[]  = { "st", NULL };
 
 #include "shiftview.c"
