d0e534533bf27ed9f7c2a83021d86188aa0db9ea
Alex Lu <alxlu@users.noreply.github.com>
2020-11-06 13:39:19 -0800

    also warp to top left corner if clients present

--- dwm.c
+++ dwm.c
@@ -2293,7 +2293,7 @@ warp(const Client *c)
 	    (c->mon->topbar && !y))
 		return;
 
-	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, c->w / 2, c->h / 2);
+	XWarpPointer(dpy, None, c->win, 0, 0, 0, 0, 0, 0);
 }
 
 Client *
