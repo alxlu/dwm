4a60b4f3c724a8f12eb1ee9be5fb0586fd3ce107
Alex Lu <alxlu@users.noreply.github.com>
2020-11-14 09:58:21 -0800

    warp pointer on focus change

--- dwm.c
+++ dwm.c
@@ -1424,10 +1424,8 @@ restack(Monitor *m)
 	}
 	XSync(dpy, False);
 	while (XCheckMaskEvent(dpy, EnterWindowMask, &ev));
-        /*
 	if (m == selmon && (m->tagset[m->seltags] & m->sel->tags) && selmon->lt[selmon->sellt] != &layouts[2])
 		warp(m->sel);
-        */
 }
 
 void
